694f053e5fdc0f2ed1c62506233ab403a45b2831
vince <vincent.cordola@hes-so.ch>
2023-12-19 18:34:31 +0100

    première base

---
+++ src/main/scala/G2048.scala
@@ -0,0 +1,151 @@
+import hevs.graphics.FunGraphics
+
+import java.awt.Color
+import java.awt.event.{KeyAdapter, KeyEvent}
+import scala.util.Random
+
+class G2048 {
+  // Element de base
+  var tabgame: Array[Array[Int]] = Array.ofDim(4, 4)
+  var peuxdeplace: Boolean = true
+  // Initialisation du tableau
+  for (i <- tabgame.indices) {
+    for (j <- tabgame(i).indices) {
+      tabgame(i)(j) = 0
+    }
+  }
+
+
+  // fungraphics
+  val fg: FunGraphics = new FunGraphics(600, 600)
+  var couCase = Color.white
+  // variable base
+  var mvmX = 0
+  var mvmY = 1
+  var lemptyX: List[Int] = List()
+  var lemptyY: List[Int] = List()
+  var nbalea = 0
+
+
+  // Début du jeux
+  tabgame(0)(0) = 2
+  // affichage premier tableau
+  affijeux()
+
+  // Utilisation des touches
+  fg.setKeyManager(new KeyAdapter() { // Will be called when a key has been pressed
+    override def keyPressed(e: KeyEvent): Unit = {
+      if (e.getKeyCode == KeyEvent.VK_RIGHT) {
+        mvmX = 1
+        mvmY = 0
+        deplajeux()
+        nouveaunbre()
+        affijeux()
+      }
+      if (e.getKeyCode == KeyEvent.VK_DOWN) {
+        mvmX = 0
+        mvmY = 1
+        deplajeux()
+        nouveaunbre()
+        affijeux()
+      }
+      if (e.getKeyCode == KeyEvent.VK_UP) {
+        mvmX = 0
+        mvmY = -1
+        deplajeux()
+        nouveaunbre()
+        affijeux()
+      }
+      if (e.getKeyCode == KeyEvent.VK_LEFT) {
+        mvmX = -1
+        mvmY = 0
+        deplajeux()
+        nouveaunbre()
+        affijeux()
+      }
+    }
+  })
+
+
+  // Déplacement jeux
+  def deplajeux(): Unit = {
+    for (z <- 0 until 2) {
+      peuxdeplace = true
+      while (peuxdeplace) {
+        for (i <- tabgame.indices) {
+          for (j <- tabgame(i).indices) {
+            // deplacement case vide
+            try {
+              if (tabgame(i)(j) != 0 && tabgame(i + mvmY)(j + mvmX) == 0) {
+                tabgame(i + mvmY)(j + mvmX) = tabgame(i)(j)
+                tabgame(i)(j) = 0
+              }
+            } catch {
+              case e => peuxdeplace = false
+            }
+            // addition de case
+            try {
+              // && tabgame(i)(j) != tabgame(i + mvmY * 2)(j + mvmX * 2) || temp marche pas
+              if (tabgame(i)(j) != 0 && tabgame(i)(j) == tabgame(i + mvmY)(j + mvmX)) {
+                tabgame(i + mvmY)(j + mvmX) = tabgame(i)(j) * 2
+                tabgame(i)(j) = 0
+              }
+            } catch {
+              case f => print("")
+            }
+          }
+        }
+      }
+    }
+  }
+
+
+  // nouveau nombre (viens après déplacement)
+  def nouveaunbre(): Unit = {
+    for (i <- tabgame.indices) {
+      for (j <- tabgame(i).indices) {
+        if (tabgame(i)(j) == 0) {
+          lemptyX = lemptyX :+ j
+          lemptyY = lemptyY :+ i
+        }
+      }
+    }
+    nbalea = Random.nextInt(lemptyX.length - 1)
+    tabgame(lemptyY(nbalea))(lemptyX(nbalea)) = 2
+    lemptyX = List()
+    lemptyY = List()
+  }
+
+
+  // affichage jeux
+  def affijeux(): Unit = {
+    println("")
+    for (i <- tabgame.indices) {
+      for (j <- tabgame(i).indices) {
+        // print(s"${tabgame(i)(j)} ")
+        tabgame(i)(j) match {
+          case 0 => couCase = Color.white
+          case 2 => couCase = Color.lightGray
+          case 4 => couCase = Color.GRAY
+          case 8 => couCase = Color.BLUE
+          case 16 => couCase = Color.CYAN
+          case 32 => couCase = Color.red
+          case 64 => couCase = Color.green
+          case 128 => couCase = Color.darkGray
+          case 256 => couCase = Color.magenta
+          case 512 => couCase = Color.orange
+          case 1024 => couCase = Color.pink
+          case 2048 => couCase = Color.yellow
+        }
+        fg.setColor(couCase)
+        fg.drawFillRect(200 + 50*j,200+50*i,50,50)
+        fg.setColor(Color.black)
+        fg.drawRect(200 + 50*j,200+50*i,50,50)
+        fg.drawFancyString(215 + 50*j,225+50*i,tabgame(i)(j).toString,Color.BLACK,20)
+
+      }
+      println("")
+    }
+  }
+
+}
--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,3 +1,3 @@
-object Game {
-
+object Game extends App {
+  var game01: G2048 = new G2048()
 }
